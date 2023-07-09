1000e6529e1b6d9478f5d62ed6ffff388d32dc80
ahmedGamalEllabban <138603828+ahmedGamalEllabban@users.noreply.github.com>
2023-07-09 12:10:37 +0300

    Update firstTask.php

--- firstTask.php
+++ firstTask.php
@@ -1,11 +1,17 @@
 <?php
-echo "<center>";
-for ($i = 1; $i <= 20; $i++) {
+
+$end = 30;
+$costum = $end;
+echo '<pre>';
+for ($i = 1; $i <= $end; $i++) {
     if ($i % 2 != 0) {
+        $costum--;
+        for ($j = $costum; $j >= 1; $j--) echo ' ';
         for ($j = 1; $j <= $i; $j++) {
             echo "*";
         }
-        echo "<br>";
+
+        echo '<br>';
     }
 }
-echo "</center>";
+echo '</pre>';
